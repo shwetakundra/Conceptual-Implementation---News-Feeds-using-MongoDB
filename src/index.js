3bb07387a4438e1ae5fc74a02497cbb2841b2997
shwetakundra <shwetakundra688@gmail.com>
2020-12-22 12:34:12 +0530

    limit offset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const MyModel=require('./connector')
+const newsArticleModel=MyModel.newsArticleModel
 
 const onePageArticleCount = 10
 
@@ -8,6 +10,16 @@ const onePageArticleCount = 10
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.get('/newFeeds',async(req,res)=>{
+   res.send(await newsArticleModel.find().skip(sanitize(req.query.offset,0)).limit(sanitize(req.query.limit,10)))    
+   })
+   const sanitize=(value,defaultvalue)=>{
+       if(value==null||value==undefined||isNaN(Number(value))){
+           return defaultvalue
+       }
+       return Number(value)
+
+   }
 
 
 
